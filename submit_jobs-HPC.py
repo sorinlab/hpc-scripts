cdd565eaebb0dd20ea0bc4a020e0b701dc03b97d
Aingty Eung <aingtyeung@yahoo.com>
2018-11-06 23:20:50 -0800

    Testing

--- submit_jobs-HPC.py
+++ submit_jobs-HPC.py
@@ -134,8 +134,9 @@ if not os.path.exists('%s.top' % (deffnm)):
 if not os.path.exists('%s.mdp' % (deffnm)):
 	print("\n***** ERROR: '%s.mdp' file not found. *****" % (deffnm))
 	missing_file_error = True
-if not os.path.exists('%s.ndx' % (deffnm)) and take_ndx == True:
+if take_ndx == True and not os.path.exists('%s.ndx' % (deffnm)):
 	print("\n***** ERROR: '%s.ndx' file not found. *****" % (deffnm))
+	missing_file_error = True
 if missing_file_error:
 	sys.exit()
 
